007a0459c646b729afeefbc466851076b599fd1c
Ubuntu <ml@mlsolution-gpu.isucce5iypxe3mglpshhcyigyd.rx.internal.cloudapp.net>
2021-03-11 13:22:49 +0000

    preprocess function to directly train sqlites from tog

--- project/[[python_package_import_name]]/dev/io/reader/sqlite.py
+++ project/[[python_package_import_name]]/dev/io/reader/sqlite.py
@@ -7,6 +7,24 @@ DEFAULT_QUERY = "SELECT {cols} FROM data"
 DEFAULT_COLS = ["*"]
 
 
+def pre_process_client(data):
+    """
+    this functions extract data and label from the tog-cli sqlites in a format that can be directly used with dialogy train
+    edit this function if you want to extract columns other than data and labels
+    """
+
+    full_data=[]
+    for i in data:
+        text = json.loads(i[1])['alternatives']
+        text_dict={"alternatives":text}
+        text_dict = json.dumps(text_dict)
+        label = json.loads(i[2])[0]['type']
+        data_tuple=(text_dict,label) 
+        full_data.append(data_tuple)
+    return full_data
+
+
+
 def read_sqlite(db, query=DEFAULT_QUERY, usecols=None):
     if not usecols:
         usecols = DEFAULT_COLS
@@ -20,5 +38,7 @@ def read_sqlite(db, query=DEFAULT_QUERY, usecols=None):
 
 
 def read_multiclass_dataset_sqlite(full_path, alias=None, **kwargs):
-    columns, data = read_sqlite(full_path, query=DEFAULT_QUERY, **kwargs)
+    _, data = read_sqlite(full_path, query=DEFAULT_QUERY, **kwargs)
+    data = pre_process_client(data)
+    columns=['data','labels'] # edit this if you want more columns
     return pd.DataFrame(data, columns=columns)
