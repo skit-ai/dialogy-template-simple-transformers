4df5e2ce0508e4ff966e71d75d380acf8b4346b5
ltbringer <amresh.venugopal@gmail.com>
2021-04-08 12:37:51 +0530

    init: project.

---
+++ project/[[python_package_import_name]]/dev/io/reader/sqlite.py
@@ -0,0 +1,24 @@
+import sqlite3
+import pandas as pd
+from tqdm import tqdm
+
+
+DEFAULT_QUERY = "SELECT {cols} FROM data"
+DEFAULT_COLS = ["*"]
+
+
+def read_sqlite(db, query=DEFAULT_QUERY, usecols=None):
+    if not usecols:
+        usecols = DEFAULT_COLS
+
+    query = query.format(cols=", ".join(usecols))
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    columns = [description[0] for description in cursor.description]
+    return columns, cursor.fetchall()
+
+
+def read_multiclass_dataset_sqlite(full_path, alias=None, **kwargs):
+    columns, data = read_sqlite(full_path, query=DEFAULT_QUERY, **kwargs)
+    return pd.DataFrame(data, columns=columns)
