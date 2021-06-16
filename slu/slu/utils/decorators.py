95a28454bed060db2aa80fe49560db00d3977567
ltbringer <amresh.venugopal@gmail.com>
2021-06-16 12:35:06 +0530

    refactor: use as per updated config class and rename.

--- slu/slu/utils/decorators.py
+++ slu/slu/utils/decorators.py
@@ -2,11 +2,21 @@ import functools
 
 from slu import constants as const
 
-def checks_usage(func):
+
+def task_guard(func):
     @functools.wraps
-    def wrapper(self, task, *args, **kwargs):
-        if (task == const.CLASSIFICATION and self.use_classification or
-            task == const.NER and self.use_ner):
-            value = func(task=task,*args, **kwargs)
+    def wrapper(self, task_name: str, *args, **kwargs):
+        supported_tasks = {const.CLASSIFICATION, const.NER}
+
+        if task_name not in supported_tasks:
+            raise ValueError(f"Task should be one of {supported_tasks}")
+
+        use_task = self.task_by_name(task_name).use
+
+        if use_task:
+            value = func(task=task_name,*args, **kwargs)
             return value
+        else:
+            raise ValueError(f"Attempting to run a {task_name} task but {func} failed,"
+            f" because config has {const.TASKS}.")
     return wrapper
