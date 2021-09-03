fd45b962ea48c20b4d677e62ea9529eb58931db1
ltbringer <amresh.venugopal@gmail.com>
2021-09-03 06:44:17 +0530

    update(train): removed ner. update(train): trains workflow instead of classifier alone.

--- slu/slu/dev/train.py
+++ slu/slu/dev/train.py
@@ -15,71 +15,29 @@ Options:
     -h --help     Show this screen.
     --version     Show version.
 """
-import shutil
+import argparse
 
-import semver
-from docopt import docopt
-from sklearn import preprocessing  # type: ignore
-from sklearn.metrics import accuracy_score  # type: ignore
-from sklearn.model_selection import train_test_split  # type: ignore
+import pandas as pd
 
 from slu import constants as const
-from slu.utils.config import Config
-from slu.utils.logger import log
+from slu.src.controller.prediction import get_workflow
+from slu.utils.config import Config, YAMLLocalConfig
+from slu.utils import logger
 
 
-def train_intent_classifier(
-    config: Config, eval_split=const.N_EVAL_SPLIT, file_format=const.CSV
-):
-    log.info("Preparing dataset.")
-    train_df = config.get_dataset(
-        const.CLASSIFICATION, const.TRAIN, file_format=file_format
-    )
+def train_intent_classifier(args: argparse.Namespace) -> None:
+    version = args.version
+    dataset = args.file
+    project_config_map = YAMLLocalConfig().generate()
+    config: Config = list(project_config_map.values()).pop()
 
-    label_encoder = preprocessing.LabelEncoder()
-    encoder = label_encoder.fit(train_df[const.LABELS])
-    train_df[const.LABELS] = encoder.transform(train_df[const.LABELS])
-    config.set_labels(const.CLASSIFICATION, const.TRAIN, label_encoder)
+    workflow = get_workflow(const.TRAIN, lang=args.lang, project=args.project)
 
-    train_df, eval_df = train_test_split(
-        train_df,
-        test_size=eval_split,
-        random_state=0,
-        stratify=train_df[[const.LABELS]],
-    )
+    logger.info("Preparing dataset.")
+    dataset = dataset or config.get_dataset(const.CLASSIFICATION, f"{const.TRAIN}.csv")
+    train_df = pd.read_csv(dataset)
 
-    model = config.get_model(const.CLASSIFICATION, const.TRAIN)
-    model_dir = config.get_model_dir(const.CLASSIFICATION, const.TRAIN)
-
-    log.info("Training started.")
-    model.train_model(
-        train_df,
-        output_dir=model_dir,
-        eval_df=eval_df,
-        acc=accuracy_score,
-    )
-
-    log.info("Saving artifacts.")
-    config.set_model_dir(const.CLASSIFICATION, const.TRAIN)
+    logger.info("Training started.")
+    workflow.train(train_df)
     config.save()
-    config.remove_checkpoints(const.CLASSIFICATION, const.TRAIN)
-
-    log.debug("Finished!")
-
-
-def train_ner_model(config: Config, file_format=const.CSV):
-    log.info("Preparing dataset.")
-    train_df = config.get_dataset(const.NER, const.TRAIN, file_format=file_format)
-    labels = sorted(train_df[const.LABELS].unique().tolist())
-    config.set_labels(const.NER, const.TRAIN, labels)
-    model = config.get_model(const.NER, const.TRAIN)
-
-    log.info("Training started.")
-    model.train_model(train_df)
-
-    log.info("Saving artifacts.")
-    config.set_model_dir(const.NER, const.TRAIN)
-    config.save()
-    config.remove_checkpoints(const.NER, const.TRAIN)
-
-    log.debug("Finished!")
+    logger.debug("Finished!")
