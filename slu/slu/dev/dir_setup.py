7511619fd01c09c8e52b11ace5851e42e61e1459
ltbringer <amresh.venugopal@gmail.com>
2021-09-03 06:40:43 +0530

    update: force flag removed

--- slu/slu/dev/dir_setup.py
+++ slu/slu/dev/dir_setup.py
@@ -20,56 +20,58 @@ and hence maintaining the uniformity of the directory structure here.
 """
 import os
 import shutil
+import argparse
 
 import semver
+
 
 from slu import constants as const
 
 
-def create_data_directory(version: str, force: bool = False) -> None:
+def create_data_directory(args: argparse.Namespace) -> None:
     """
-    Create subdirectories.
+    Create sub directories.
 
-    1. This function will check if `version` is a valid semver.
-    2. Create a directory that contains a structure if it doesn't already exist.
-        Unless `force=True`.
+    This function will check if `version` is a valid semver.
 
     Args:
         version (str): Semver for the dataset, model and metrics.
-        force (bool, optional): Flag to overwrite existing directory. Defaults to False.
     """
+    version = args.version
+
     # This will raise an exception for invalid semver. So we don't have to catch it.
     semver.VersionInfo.parse(version)
 
     base_module_path = os.path.join(const.DATA, version)
-    depth_level_1 = [const.CLASSIFICATION, const.NER]
+    depth_level_1 = [const.CLASSIFICATION]
     depth_level_2 = [const.DATASETS, const.METRICS, const.MODELS]
 
     for subdir in depth_level_1:
         for childdir in depth_level_2:
-            # `exist_ok=False` is the default argument, it raises OSError if the path already exists.
             os.makedirs(
-                os.path.join(base_module_path, subdir, childdir), exist_ok=force
+                os.path.join(base_module_path, subdir, childdir), exist_ok=True
             )
 
 
-def copy_data_directory(copy_from: str, copy_to: str, force: bool = False) -> None:
+def copy_data_directory(args: argparse.Namespace) -> None:
     """
     Copy subdirectory.
 
     1. This function will check `copy_from` and `copy_to` are valid semver.
     2. This function will check `copy_to` doesn't already exist.
-    3. Unless force = True, this function will not overwrite existing directory.
 
     Args:
         copy_from (str): semver -> Source directory.
         copy_to (str): semver -> Destination directory.
-        force (bool, optional): Flag to overwrite existing directory. Defaults to False.
     """
+    copy_from = args.source
+    copy_to = args.dest
+    force = args.force
+
     # This will raise an exception for invalid semver. So we don't have to catch it.
     semver.VersionInfo.parse(copy_from)
     semver.VersionInfo.parse(copy_to)
 
     source = os.path.join(const.DATA, copy_from)
     destination = os.path.join(const.DATA, copy_to)
-    shutil.copytree(source, destination, dirs_exist_ok=force)
+    shutil.copytree(source, destination)
