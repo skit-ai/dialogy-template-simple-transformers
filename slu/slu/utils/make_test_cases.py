ef8ec9ea7c6d16a59f710a5f5ad89ef893a2715c
ltbringer <amresh.venugopal@gmail.com>
2021-09-03 06:51:05 +0530

    add: util to create test cases.

---
+++ slu/slu/utils/make_test_cases.py
@@ -0,0 +1,42 @@
+import os
+import json
+import yaml
+import hashlib
+
+import slu.constants as const
+
+
+def build_test_case(inputs_, outputs) -> None:
+    """
+    Build a test case from the given input and output data.
+
+    :param inputs_: The input data for the test case.
+    :param outputs: The output data for the test case.
+    :return: A dictionary representing the test case.
+    """
+    message = hashlib.sha256()
+    if os.environ.get("ENVIRONMENT") == const.PRODUCTION:
+        return
+
+    with open(os.path.join("tests", "test_controller", "test_cases.yaml"), "r") as handle:
+        test_cases = yaml.load(handle, Loader=yaml.SafeLoader)
+
+    if not test_cases:
+        test_cases = []
+
+    message.update(str.encode(json.dumps(inputs_)))
+    signature = message.hexdigest()
+
+    if test_cases:
+        last_signature = test_cases[-1]["signature"]
+        if last_signature == signature:
+            return
+
+    with open(os.path.join("tests", "test_controller", "test_cases.yaml"), "w") as handle:
+        test_cases.append({
+            "input": inputs_,
+            "output": json.loads(json.dumps(outputs)),
+            "signature": signature
+        })
+
+        yaml.dump(test_cases, handle, default_flow_style=False)
