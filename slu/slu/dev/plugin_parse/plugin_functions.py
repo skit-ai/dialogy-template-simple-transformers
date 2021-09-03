e6c9ac53e298c14253349c9a9ab5f5a6c474ba98
ltbringer <amresh.venugopal@gmail.com>
2021-09-03 06:44:45 +0530

    fix: plugin updates simplified.

--- slu/slu/dev/plugin_parse/plugin_functions.py
+++ slu/slu/dev/plugin_parse/plugin_functions.py
@@ -2,30 +2,23 @@ import os
 from typing import Any
 
 from dialogy.workflow import Workflow
+from slu import constants as const
 
 
 def access(node: str, *attributes: str):
     def read(workflow):
         workflow_io = getattr(workflow, node)
         return (workflow_io[attribute] for attribute in attributes)
-
     return read
 
 
-def mutate(node: str, *attributes: str):
-    attribute = attributes[0]
-
+def mutate(node: str, attribute: str, action=const.EXTEND):
     def write(workflow: Workflow, value: Any):
         workflow_io = getattr(workflow, node)
-        container = workflow_io[attribute]
-        if isinstance(container, list):
-            if isinstance(value, list):
-                container += value
-            else:
-                container.append(value)
-        else:
+        if action == const.REPLACE:
             workflow_io[attribute] = value
-
+        elif action == const.EXTEND:
+            workflow_io[attribute].extend(value)
     return write
 
 
