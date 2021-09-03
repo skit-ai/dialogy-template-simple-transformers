3ed826988697f41f5642f3c696a5711e010fdcfe
ltbringer <amresh.venugopal@gmail.com>
2021-09-04 02:06:11 +0530

    add(test): save error report.

--- slu/slu/dev/test.py
+++ slu/slu/dev/test.py
@@ -12,18 +12,20 @@ Options:
     -h --help   Show this screen.
     --version   Show version.
 """
+import json
 import argparse
 
 import pandas as pd
-import semver
+from tqdm import tqdm
 from dialogy.utils import create_timestamps_path
 from sklearn.metrics import classification_report, confusion_matrix
 from tabulate import tabulate
 
 from slu import constants as const
-from slu.src.controller.prediction import get_workflow
+from slu.src.controller.prediction import get_predictions
 from slu.utils import logger
 from slu.utils.config import Config, YAMLLocalConfig
+from slu.dev.version import check_version_save_config
 
 
 def zoom_out_labels(labels):
@@ -64,6 +66,20 @@ def make_classification_report(
         )
     )
 
+def make_errors_report(
+    config: Config, version: str, test_df: pd.DataFrame, predictions_df: pd.DataFrame
+):
+    logger.info(f"{test_df.head()}")
+    test_df_ = test_df.copy()
+    merged_df = pd.merge(test_df_, predictions_df, on="data_id", suffixes=("_test", "_pred"))
+    errors_df = merged_df[merged_df[f"{const.INTENT}_test"] != merged_df[f"{const.INTENT}_pred"]].copy()
+    errors_df.to_csv(
+        create_timestamps_path(
+            config.get_metrics_dir(const.CLASSIFICATION, version=version),
+            "error_report.csv",
+        )
+    )
+
 
 def make_confusion_matrix(
     config: Config, version: str, test_df: pd.DataFrame, predictions_df: pd.DataFrame
@@ -83,21 +99,49 @@ def make_confusion_matrix(
 
 
 def test_classifier(args: argparse.Namespace):
+    """
+    Evaluate the workflow with all the embedded plugins.
+
+    Plugins can be evaluated individually for fine-tuning but since there are interactions
+    between them, we need to evaluate them all together. This helps in cases where these interactions
+    are a cause of a model's poor performance.
+
+    This method doesn't mutate the given test dataset, instead we produce results with the same `id_`
+    so that they can be joined and studied together.
+    """
     version = args.version
     dataset = args.file
+    lang = args.lang
     project_config_map = YAMLLocalConfig().generate()
     config: Config = list(project_config_map.values()).pop()
-    if version:
-        semver.VersionInfo.parse(version)
-        config.version = version
-        config.save()
+    check_version_save_config(config, version)
 
-    workflow = get_workflow(const.TEST, lang=args.lang, project=args.project)
-
+    predict_api = get_predictions(const.PRODUCTION, config=config, debug=False)
     dataset = dataset or config.get_dataset(const.CLASSIFICATION, f"{const.TEST}.csv")
     test_df = pd.read_csv(dataset)
 
     logger.info("Running predictions")
-    predictions_df = workflow.prediction_labels(test_df, id_="data_id")
+    predictions = []
+    logger.disable("slu")
+
+    for i, row in tqdm(test_df.iterrows(), total=test_df.shape[0]):
+        output = predict_api(**{
+            const.ALTERNATIVES: json.loads(row[const.ALTERNATIVES]),
+            const.CONTEXT: {},
+            const.LANG: lang,
+            "ignore_test_case": True,
+        })
+        intents = output.get(const.INTENTS, [])
+        predictions.append(
+            {
+                "data_id": row["data_id"],
+                const.INTENT: intents[0][const.NAME] if intents else "_no_preds_",
+                const.SCORE: intents[0][const.SCORE] if intents else 0,
+            }
+        )
+
+    logger.enable("slu")
+    predictions_df = pd.DataFrame(predictions)
+    make_errors_report(config, version, test_df, predictions_df)
     make_classification_report(config, version, test_df, predictions_df)
     make_confusion_matrix(config, version, test_df, predictions_df)
