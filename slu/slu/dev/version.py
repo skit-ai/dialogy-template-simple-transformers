87d219170c095922e3304a06cf933b5dd3aaf739
ltbringer <amresh.venugopal@gmail.com>
2021-09-05 03:25:03 +0530

    add: version log for all commands.

--- slu/slu/dev/version.py
+++ slu/slu/dev/version.py
@@ -5,6 +5,6 @@ from slu.utils import logger
 def check_version_save_config(config, version):
     if version:
         semver.VersionInfo.parse(version)
-        logger.info(f"Using version: {version}.")
         config.version = version
         config.save()
+    logger.info(f"Using version {version or config.version}")
