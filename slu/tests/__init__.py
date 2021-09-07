c7e0e791b9bc4198880ae5948715e9bd6969c993
ltbringer <amresh.venugopal@gmail.com>
2021-09-08 00:30:17 +0530

    fix: tests fail when no cases added.

--- slu/tests/__init__.py
+++ slu/tests/__init__.py
@@ -7,4 +7,6 @@ def load_tests(test_type, current_path):
     test_dir = pathlib.Path(current_path).parent
     test_cases_path = os.path.join(test_dir, f"test_{test_type}.yaml")
     with open(test_cases_path, "r") as handle:
-        return yaml.load(handle, Loader=yaml.SafeLoader).values()
+        test_cases = yaml.load(handle, Loader=yaml.SafeLoader)
+        if isinstance(test_cases, dict):
+            return test_cases.values()
