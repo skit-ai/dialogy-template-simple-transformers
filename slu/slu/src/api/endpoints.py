9f04cb7165cc0dba60d73616d19f9bde8da5e7d4
Biswaroop Bhattacharjee <biswaroop08@gmail.com>
2022-03-28 23:07:31 +0530

    add: shifted template from flask-uwsgi to fastapi-uvicorn server (WSGI -> ASGI)

--- slu/slu/src/api/endpoints.py
+++ slu/slu/src/api/endpoints.py
@@ -2,25 +2,24 @@ import os
 import traceback
 from typing import Any, Dict, List
 
+from fastapi.responses import JSONResponse
+
 import sentry_sdk
-from flask import jsonify, request
-from sentry_sdk.integrations.flask import FlaskIntegration
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 from slu import constants as const
-from slu.src.api import app
-from slu.src.api.thread_safe import ThreadSafePredictAPI
+from slu.src.api import app, Input
+from slu.src.controller.prediction import get_predictions
 from slu.utils import error_response
 from slu.utils.config import Config, YAMLLocalConfig
 from slu.utils.sentry import capture_exception
 
-
 CONFIG_MAP = YAMLLocalConfig().generate()
-
+PREDICT_API = get_predictions(const.PRODUCTION, ignore_test_case=True)
 
 if os.environ.get(const.ENVIRONMENT) == const.PRODUCTION:
     sentry_sdk.init(
         dsn=os.environ["SENTRY_DSN"],
-        integrations=[FlaskIntegration()],
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production.
@@ -30,64 +29,66 @@ if os.environ.get(const.ENVIRONMENT) == const.PRODUCTION:
         # something more human-readable.
         # release="myapp@1.0.0",
     )
+    app.add_middleware(SentryAsgiMiddleware)
 
 
-@app.route("/", methods=["GET"])
-def health_check():
+@app.get("/")
+async def health_check():
     """
     Get server status health.
 
     The purpose of this API is to help other people/machines know liveness of the application.
     """
-    return jsonify(
-        status="ok",
-        response={"message": "Server is up."},
-    )
+
+    return JSONResponse(dict(
+            status="ok",
+            response={"message": "Server is up."},
+            ),
+        status_code=200)
 
 
-@app.route("/predict/<lang>/<model_name>/", methods=["POST"])
-def slu(lang: str, model_name: str):
+@app.post("/predict/{lang}/{model_name}/")
+async def slu(lang: str, model_name: str, payload: Input):
     """
     Get SLU predictions.
-
     Produces a json response containing intents and entities.
     """
+    request = payload.dict()
     config: Config = list(CONFIG_MAP.values()).pop()
+
     if lang not in config.get_supported_languages():
         return error_response.invalid_language(lang)
 
-    if not isinstance(request.json, dict):
-        return error_response.invalid_request(request.json)
-
-    if not (const.ALTERNATIVES in request.json or const.TEXT in request.json):
-        return error_response.invalid_input(request.json)
+    if not request[const.ALTERNATIVES] and not request[const.TEXT]:
+        return error_response.invalid_input(request)
 
     try:
-        utterance: Any = request.json.get(const.ALTERNATIVES) or request.json.get(
-            const.TEXT
-        )
+        utterance: Any = request[const.ALTERNATIVES] or request[const.TEXT]
+        context: Dict[str, Any] = request[const.CONTEXT]
+        history: List[Dict[str, Any]] = request[const.HISTORY]
+        intents_info: List[Dict[str, Any]] = request[const.INTENTS_INFO]
 
-        context: str = request.json.get(const.CONTEXT) or {}  # type: ignore
-        intents_info: List[Dict[str, Any]] = request.json.get(const.INTENTS_INFO) or []
-        history: List[Any] = request.json.get(const.HISTORY) or []
-
-        with ThreadSafePredictAPI(
-            utterance,
-            lang,
-            config,
-            context=context,
-            intents_info=intents_info,
-            history=history
-        ) as response:
+        try:
+            response = PREDICT_API(
+                alternatives=utterance,
+                context=context,
+                intents_info=intents_info,
+                history=history,
+                lang=lang,
+            )
             history.append(response)
-            return jsonify(status="ok", response=response, history=history), 200
+            return JSONResponse(dict(status="ok", response=response, history=history), status_code=200)
+
+        except OSError as os_error:
+            return error_response.missing_models(os_error)
 
     except Exception as exc:
         # Update this section to:
         # 1. Handle specific errors
         # 2. provide user-friendly messages. The current is developer friendly.
-        capture_exception(exc, ctx="api", message=request.json)
-        return jsonify({"message": str(exc), "cause": traceback.format_exc()}), 200
+        # capture_exception(exc, ctx="api", message=request.json)
+        capture_exception(exc, ctx="api", message=request)
+        return JSONResponse({"message": str(exc), "cause": traceback.format_exc()}, status_code=200)
 
 
 if __name__ == "__main__":
--- slu/slu/utils/error_response.py
+++ slu/slu/utils/error_response.py
@@ -1,100 +1,101 @@
-from flask import json, jsonify, request
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 from slu import constants as const
 
 
 def invalid_language(lang):
-    return (
-        jsonify(
+    return JSONResponse (
+        content=jsonable_encoder(
             {
                 "message": "Invalid request.",
                 "cause": f"Language '{lang}' is not supported.",
             }
         ),
-        400,
+        status_code=400,
     )
 
 
 def invalid_request(req):
-    return (
-        jsonify(
+    return JSONResponse (
+        content=jsonable_encoder(
             {
                 "message": "Invalid request.",
                 "cause": f"Post body should be a dictionary, received {type(req)}.",
             }
         ),
-        400,
+        status_code=400,
     )
 
 
 def invalid_input(req):
-    return (
-        jsonify(
+    return JSONResponse (
+        content=jsonable_encoder(
             {
                 "message": "Invalid request.",
                 "cause": f"Post body should have either of these keys: {const.TEXT}, {const.ALTERNATIVES}."
                 f" Instead got\n{req}",
             }
         ),
-        400,
+        status_code=400,
     )
 
 
 def missing_project_name(project_name):
-    return (
-        jsonify(
+    return JSONResponse (
+        content=jsonable_encoder(
             {
                 "message": "Project not found.",
                 "cause": f"config for project_name: {project_name} is not present.",
             }
         ),
-        404,
+        status_code=400,
     )
 
 
 def invalid_initialization(client_name, model_name):
-    return (
-        jsonify(
+    return JSONResponse (
+        content=jsonable_encoder(
             {
                 "message": "Missing config.",
                 "cause": f"Server started but config for {client_name} -- {model_name} was not loaded.",
             }
         ),
-        500,
+        status_code=500,
     )
 
 
 def config_upload_required(plugin):
-    return (
-        jsonify(
+    return JSONResponse (
+        content=jsonable_encoder(
             {
                 "message": "Plugin needs config file.",
                 "cause": f"Plugin {plugin} needs a config file.",
             }
         ),
-        400,
+        status_code=400,
     )
 
 
 def unknown_plugin(plugin):
-    return (
-        jsonify(
+    return JSONResponse (
+        content=jsonable_encoder(
             {
                 "message": "No schema available for plugin.",
                 "cause": f"Plugin {plugin} does not have a schema parser defined.",
             }
         ),
-        400,
+        status_code=400,
     )
 
 
 def missing_models(message):
-    return (
-        jsonify(
+    return JSONResponse (
+        content=jsonable_encoder(
             {
                 "message": message,
                 "cause": "Possibly missed the training step or it wasn't planned.",
             }
         ),
-        500,
+        status_code=500,
     )
