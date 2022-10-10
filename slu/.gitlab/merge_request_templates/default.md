6718db30ed5964be18a42e2d5a284a7b420568a8
Daksh Varshneya <dakshvarshneya@admins-MacBook-Pro-7.local>
2022-10-10 11:29:57 +0530

    add mr template and fixes for tests

---
+++ slu/.gitlab/merge_request_templates/default.md
@@ -0,0 +1,10 @@
+## Changes proposed (describe in as much detail as you can)
+1. What problem does it solve?
+2. What alternative solutions were thought of? (It's okay if there were none, but if there were, they should be briefly made explicit.)
+
+## Checklist
+- [ ] Wrote unit tests for any new custom plugins or changes to existing custom plugins.
+- [ ] Added necessary test cases in test_cases.yaml.
+- [ ] Data and models pushed to DVC (should ideally happen through the pre-commit hook)
+- [ ] Code Formatted - `make lint` (should ideally happen through the pre-commit hook)
+- [ ] Any change needed in State repo done via request to DevOps team.
--- slu/scripts/run_test_cases_on_service.py
+++ slu/scripts/run_test_cases_on_service.py
@@ -1,6 +1,7 @@
 import requests
 import argparse
-import os, yaml
+from pprint import pprint
+import os, yaml, json
 
 from slu import constants as const
 
@@ -19,18 +20,97 @@ def load_tests(test_cases_path):
             return zip(test_cases.keys(), test_cases.values())
 
 
+def verify_intents(key, predicted_output, expected_output):
+    pred_intent_name = predicted_output[const.INTENTS][0][const.NAME]
+    true_intent_name = expected_output[const.INTENTS][0][const.NAME]
+
+    assert pred_intent_name == true_intent_name, f"{key} intent name doesn't match!"
+
+
+def verify_slots(key, predicted_output, expected_output):
+    pred_slots = {
+        slot[const.NAME]: slot
+        for slot in predicted_output[const.INTENTS][0][const.SLOTS]
+    }
+    true_slots = {
+        slot[const.NAME]: slot
+        for slot in expected_output[const.INTENTS][0][const.SLOTS]
+    }
+
+    assert len(pred_slots) == len(true_slots), f"{key} slot-size doesn't match!"
+    for pred_slot, true_slot in zip(pred_slots, true_slots):
+        pred_slot_values = pred_slots[pred_slot][const.VALUES]
+        true_slot_values = true_slots[true_slot][const.VALUES]
+        assert len(pred_slot_values) == len(
+            true_slot_values
+        ), f"{key} - Values for slot={true_slot} doesn't match!"
+        for pred_slot_value, true_slot_value in zip(pred_slot_values, true_slot_values):
+            assert (
+                pred_slot_value[const.TYPE] == true_slot_value[const.TYPE]
+            ), f"{key} - Type for slot={true_slot_value[const.TYPE]} doesn't match!"
+            assert (
+                pred_slot_value[const.VALUE] == true_slot_value[const.VALUE]
+            ), f"{key} - Values for slot={true_slot_value[const.TYPE]} doesn't match!"
+
+
+def verify_entities(key, predicted_output, expected_output):
+    pred_entities = {
+        entity[const.TYPE]: entity for entity in predicted_output[const.ENTITIES]
+    }
+    true_entities = {
+        entity[const.TYPE]: entity for entity in expected_output[const.ENTITIES]
+    }
+
+    assert len(pred_entities) == len(true_entities), (
+        f"{key} - number of predicted "
+        f"entities does not match number "
+        f"of expected entities"
+    )
+
+    for expected_entity_type, expected_entity_val in true_entities.items():
+        pred_entity_val = pred_entities.get(expected_entity_type)
+
+        assert pred_entity_val, (
+            f"{key} - {expected_entity_type} entity "
+            f"type not found in predicted entities."
+        )
+        assert expected_entity_val.get(const.RANGE, {}).get(
+            const.START
+        ) == pred_entity_val.get(const.RANGE, {}).get(const.START), (
+            f"{key} - predicted start index for {expected_entity_type} not "
+            f"the same as expected start index."
+        )
+        assert expected_entity_val.get(const.RANGE, {}).get(
+            const.END
+        ) == pred_entity_val.get(const.RANGE, {}).get(const.END), (
+            f"{key} - predicted end index for {expected_entity_type} not "
+            f"the same as expected end index."
+        )
+        assert expected_entity_val.get(const.PARSERS) == pred_entity_val.get(
+            const.PARSERS
+        ), (
+            f"{key} - predicted parsers for {expected_entity_type} is "
+            f"not the same as the expected parsers."
+        )
+        assert expected_entity_val.get(const.VALUE) == pred_entity_val.get(
+            const.VALUE
+        ), (
+            f"{key} - predicted value for {expected_entity_type} is "
+            f"not the same as the expected value."
+        )
+
+
 def execute_tests(test_cases, args):
 
     for key, payload in test_cases:
         input_ = payload["input"]
-        output = payload["output"]
+        expected_output = payload["output"]
 
         response = make_request(args.url, input_)
 
-        pred_intent_name = response["response"][const.INTENTS][0][const.NAME]
-        true_intent_name = output[const.INTENTS][0][const.NAME]
-
-        assert pred_intent_name == true_intent_name, f"{key} intent name doesn't match!"
+        verify_intents(key, response.get("response"), expected_output)
+        verify_entities(key, response.get("response"), expected_output)
+        verify_slots(key, response.get("response"), expected_output)
 
 
 def main(args):
